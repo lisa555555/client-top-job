ba849583b5d2141e610908839dbddf2537e31e89
lisa555555 <minhtrungkem@gmail.com>
2022-05-28 05:12:28 +0700

    j zay

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '~/pages/Home';
+import Login from '~/pages/Login';
+import Register from '~/pages/Register';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Login" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/pages/Login/index.js
@@ -0,0 +1,5 @@
+function Login() {
+    return <h2>Logoin page</h2>;
+}
+
+export default Login;
